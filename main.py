a0f7041d9e3130d9fff47bef909d22448bb8fc2d
Pascall De Creator <pascall.de.creator@gmail.com>
2023-05-12 23:08:23 +0000

    Document Speech Funtion

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import speech_recognition as sr
 from gtts import gTTS
 
 def speak(text):
+	"""
+	This function takes in input text, generates an audio file from it and plays the sound
+	*Text input can only be recognised if it is in english*
+	"""
 	tts = gTTS(text=text, lang="en")
 	filename = "voice.mp3"
 	tts.save(filename)
