198e554e2be8cf9d89c42fb3584a8c2d47a716dd
Pascall De Creator <pascall.de.creator@gmail.com>
2023-05-22 19:44:35 +0000

    Create Test Main Function

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ async def speak(text : str) -> dict:
 		return { 'code' : 0, 'error': None }
 	except Exception as e:
 		return { 'code' : 1, 'error': e }
-	
 
 async def listen() -> dict:
 	"""
@@ -82,3 +81,26 @@ async def respond(prompt : str) -> dict:
 		return { 'res': data, 'code': 1, 'error': e }
 	
 	return { 'res': data, 'code': 0, 'error': None }
+
+async def main():
+	while True:
+		listen_task = asyncio.create_task(listen())
+		result = await listen_task
+
+		if result['code'] == 0:
+			print(result['res'])
+			response_task = asyncio.create_task(respond(result['res']))
+			result = await response_task
+			if result['code'] == 0:
+				print(result['res'])
+				speech_task = asyncio.create_task(speak(result['res']))
+				result = await speech_task
+				if result['code'] == 1:
+					print(result['error'])
+			else:
+				print(result['error'])
+		else:
+			print(result['error'])
+
+if __name__ == '__main__':
+	asyncio.run(main())
